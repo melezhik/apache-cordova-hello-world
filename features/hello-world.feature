dc9823774c72de53f94f70148e7405a8fc7be378
Alexey Melezhik <melezhik@gmail.com>
2018-02-06 13:47:42 +0300

    add features

---
+++ features/hello-world.feature
@@ -0,0 +1,5 @@
+Feature: This is dummy test
+
+Scenario: This is dummy scenario
+  Given I run application
+  
---
+++ features/step_definitions/step.rb
@@ -0,0 +1,4 @@
+Given("I run application") do
+  #pending # Write code here that turns the phrase above into concrete actions
+  puts "I am OK"
+end
